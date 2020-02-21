0fe1186498f2e93f90df5c76ee698813ad006f64
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2020-02-21 16:32:45 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+const PCancelable = require('p-cancelable');
+
+const action = PCancelable.fn(async ({config, convert, exportOptions, inputPath, outputPath}, onCancel) => {
+	const {barHeight, barLocation, barColor} = config.store;
+	const barY = barLocation === 'Top' ? `${barHeight}-H` : `H-${barHeight}`;
+
+	const process = convert([
+		'-i',
+		inputPath,
+		'-filter_complex',
+		`[0]drawbox=h=ih:w=iw:t=fill:c=${barColor}[rectangle];[0][rectangle]overlay=x=-W+W*(t/${exportOptions.duration}):y=${barY}[output]`,
+		'-map',
+		'[output]',
+		outputPath
+	], 'Adding progress bar');
+
+	onCancel(() => {
+		process.cancel();
+	});
+
+	return process;
+});
+
+const config = {
+	barHeight: {
+		title: 'Height',
+		type: 'number',
+		minimum: 1,
+		default: 5,
+		required: true
+	},
+	barLocation: {
+		title: 'Location',
+		type: 'string',
+		enum: ['Top', 'Bottom'],
+		default: 'Top',
+		required: true
+	},
+	barColor: {
+		title: 'Color',
+		description: 'Color of the bar in Hex format',
+		type: 'string',
+		regexp: {
+			pattern: /^((0x)|#)([0-9A-F]{8}|[0-9A-F]{6})$/.source,
+			flags: 'i'
+		},
+		required: true,
+		default: '#007aff'
+	}
+};
+
+const progressBar = {
+	title: 'Progress Bar',
+	config,
+	action
+};
+
+exports.editServices = [progressBar];
---
+++ readme.md
@@ -0,0 +1,13 @@
+# kap-progress-bar [![Build Status](https://travis-ci.org/karaggeorge/kap-progress-bar.svg?branch=master)](https://travis-ci.org/karaggeorge/kap-progress-bar)
+
+> [Kap](https://github.com/wulkano/kap) plugin - Add a progress bar to your recordings
+
+
+## Install
+
+In the `Kap` menu, go to `Preferences…`, select the `Plugins` pane, find this plugin, and toggle it.
+
+
+## Usage
+
+In the editor, click the `+` button, select this edit plugin, configure and export.
